358598b759be4e918fe08847587abd7040a3b111
Yago Rodrigues <yagohsro@gmail.com>
2017-04-06 16:13:38 -0300

    Commit de Alteracoes de Format na Tabela

--- src/main/java/com/tdsis/one04/TabelaPriceThread.java
+++ src/main/java/com/tdsis/one04/TabelaPriceThread.java
@@ -1,6 +1,7 @@
 package com.tdsis.one04;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.Scanner;
 import java.util.function.BiConsumer;
 
@@ -28,17 +29,28 @@ public class TabelaPriceThread implements Runnable {
 		BigDecimal amortizacao = new BigDecimal(0);
 		BigDecimal saldoDevedor = valorInicial;
 		System.out.println(
-				"Num Prestação -------- Valor Prestação -------- Juros --------- Amortização ---------- Saldo Devedor");
+				"Num Prestação -------- Valor Prestação ----------------- Juros ----------------- Amortização ------------ Saldo Devedor");
 		for (int i = 0; i <= qtdPrestacoes; i++) {
 			System.out.println(
-					"----" + i + "---------------------" + valorPrestacoes.setScale(2, BigDecimal.ROUND_CEILING)
-							+ "-------------------" + juros.setScale(2, BigDecimal.ROUND_HALF_DOWN)
-							+ "-------------------" + amortizacao.setScale(2, BigDecimal.ROUND_CEILING) + "------------"
-							+ saldoDevedor.setScale(2, BigDecimal.ROUND_HALF_DOWN));
+					"----" + i + "---------------------" + NumberFormat.getCurrencyInstance().format(valorPrestacoes.setScale(2, BigDecimal.ROUND_CEILING))
+							+ "-------------------" + NumberFormat.getCurrencyInstance().format(juros.setScale(2, BigDecimal.ROUND_HALF_DOWN))
+							+ "-------------------" + NumberFormat.getCurrencyInstance().format(amortizacao.setScale(2, BigDecimal.ROUND_CEILING)) + "------------"
+							+ NumberFormat.getCurrencyInstance().format(saldoDevedor.setScale(2, BigDecimal.ROUND_HALF_DOWN)));
 			juros = saldoDevedor.multiply(new BigDecimal(taxa / 100));
 			amortizacao = valorPrestacoes.subtract(juros);
 			saldoDevedor = saldoDevedor.subtract(amortizacao);
 		}
+		BigDecimal valorPago = valorPrestacoes.multiply(new BigDecimal(qtdPrestacoes));
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------");
+		System.out.println(
+				"Valor Pago ----------------"+NumberFormat.getCurrencyInstance().format(valorPago.setScale(2, BigDecimal.ROUND_HALF_EVEN))+"-----------------------------------------------------------------");
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------");
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------");
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------");
 	}
 
 }
